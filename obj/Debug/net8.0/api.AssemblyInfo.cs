041d91fb231ccc3f317d8685d569622b9e7ebd03
Usama Bashir <buttuxama@gmail.com>
2026-04-08 14:29:42 +0200

    Added Identity and JWT.

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd99044930c514e4f15348ff1d0b6e63704da12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2fa8ae26fe0d7f64b31fe8a325f5c4bcecd507c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
